46e862fbf5a595463fc5b278870384901abd9a7c
YoverSJ <ysantiago@geosatelital.com.pe>
2023-06-19 14:02:23 -0500

    Se crearon e implementaron los metodos index y show del controlador Articles, ademas se creo las vistas para esos metodos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,12 @@ class ArticlesController < ApplicationController
         render json: @article
     end
 
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   get "articles/new", to: "articles#new"
   post "articles", to: "articles#create"
+  get "articles", to: "articles#index"
+  get "articles/:id", to: "articles#show", as: :article
 
 end
