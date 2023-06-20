0043ec559bc747a5b8d83f7b964415045eb9556f
YoverSJ <ysantiago@geosatelital.com.pe>
2023-06-19 20:21:16 -0500

    relacion muchos a muchos de los modelos category y article

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,7 @@ class Article < ApplicationRecord
 
     belongs_to :user
 
+    has_many :has_categories
+    has_many :categories, through: :has_categories
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+
+    has_many :has_categories
+    has_many :articles, through: :has_categories
+    
 end
