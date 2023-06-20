3882f6fc5ec5aa3aa29cbcbaa019b59cba7241d0
YoverSJ <ysantiago@geosatelital.com.pe>
2023-06-19 19:42:53 -0500

    generamos un crud para categorias con la propiedad scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_225429) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_004025) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -56,6 +56,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_225429) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
